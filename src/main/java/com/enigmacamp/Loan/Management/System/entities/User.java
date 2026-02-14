cee86526dc253b257b590704344061e8634d9a1c
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-02-14 11:36:43 +0700

    feat: create User entity with Role Enum

--- src/main/java/com/enigmacamp/Loan/Management/System/entities/User.java
+++ src/main/java/com/enigmacamp/Loan/Management/System/entities/User.java
@@ -1,9 +1,46 @@
 package com.enigmacamp.Loan.Management.System.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public enum Role {
+        ROLE_CUSTOMER,
+        ROLE_ADMIN
+    }
 }
