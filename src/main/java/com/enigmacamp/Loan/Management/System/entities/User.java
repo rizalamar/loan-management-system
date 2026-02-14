b19505f4f6dcea85349548bcbf761242a5fdecb8
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-02-14 11:25:31 +0700

    add: lombok and validation depedencies

---
+++ src/main/java/com/enigmacamp/Loan/Management/System/entities/User.java
@@ -0,0 +1,9 @@
+package com.enigmacamp.Loan.Management.System.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+}
